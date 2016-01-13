ddccfecab6dcf24b4c8bd3399fffd8f254e71053
Therry van Neerven <therryvanneerven@gmail.com>
2016-01-13 15:37:23 +0100

    Removed hook related to my default theme

--- init.el
+++ init.el
@@ -32,14 +32,6 @@
   (if (fboundp 'toggle-frame-fullscreen)
       (toggle-frame-fullscreen)))
 
-
-(add-hook 'after-make-frame-functions
-          (lambda (frame)
-            (let ((mode (if (display-graphic-p frame) 'light 'dark)))
-              (set-frame-parameter frame 'background-mode mode)
-              (set-terminal-parameter frame 'background-mode mode))
-            (enable-theme 'solarized)))
-
 (add-hook 'after-init-hook 'ui-after-init)
 (set-frame-font "Hack:pixelsize=12:weight=normal:slant=normal:width=normal:spacing=100:scalable=true")
 
